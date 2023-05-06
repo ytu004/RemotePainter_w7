02369fc34cd8911f20dbf7a82abf7d594d19ddfc
ytu004 <125243182+ytu004@users.noreply.github.com>
2023-05-07 00:49:30 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,189 +1,74 @@
-// Create connection to Node.JS Server
-const socket = io();
-let rSlider, gSlider, bSlider, colorSwatch, sizeSlider;
-let r = 255;
-let g = 0;
-let b = 100;
-let bSize = 30;
+let paint = [];
 
-let canvas;
-let gui; 
-let drawIsOn = false;
+let prevPointer = [  [    { x: 0, y: 0 },    { x: 0, y: 0 },    { x: 0, y: 0 },    { x: 0, y: 0 }  ],
+  [    { x: 0, y: 0 },    { x: 0, y: 0 },    { x: 0, y: 0 },    { x: 0, y: 0 }  ]
+];
 
-let recievedMouseX = 0;
-let recievedMouseY = 0;
-
-let button;
+const fingertips = [8, 12, 16, 20];
 
 function setup() {
-  //Currently we make other people's drawing fit into our canvas, so when on portrait resolutions vs landscape things will look a little different/distorted
-  //ratio fix... but then need to make a bunch of other UX decisions like whether you zoom into the canvas or center it somehow
-  // if(windowWidth > windowHeight){
-  //   canvas = createCanvas(windowWidth, windowWidth);
-  // }else{
-  //   canvas = createCanvas(windowHeight, windowHeight);
-  // }
-
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("sketch-container"); 
-  canvas.mousePressed(canvasMousePressed);
-  canvas.touchStarted(canvasTouchStarted);
-
-  gui = select("#gui-container");
-  gui.addClass("open");//forcing it open at the start, remove if you want it closed
-
-  colorSwatch = createDiv("");
-  colorSwatch.parent(gui);
-  colorSwatch.addClass("color-swatch");
-
-  rSlider = createSlider(0, 255, r);
-  rSlider.parent(gui);
-  rSlider.addClass("slider");
-  gSlider = createSlider(0, 255, g);
-  gSlider.parent(gui);
-  gSlider.addClass("slider");
-  bSlider = createSlider(0, 255, b);
-  bSlider.parent(gui);
-  bSlider.addClass("slider");
-  sizeSlider = createSlider(1,100, bSize);
-  sizeSlider.parent(gui);
-  sizeSlider.addClass("slider");
-
-
-  rSlider.input(handleSliderInputChange);
-  gSlider.input(handleSliderInputChange);
-  bSlider.input(handleSliderInputChange);
-  sizeSlider.input(handleSliderInputChange);
+  sketch = createCanvas(1,1);
   
-  //call this once at start so the color matches our mapping to slider width
-  handleSliderInputChange();
-
-  button = createButton(">");
-  button.addClass("button");
-
-  //Add the play button to the parent gui HTML element
-  button.parent(gui);
+  const colorMap = [    [color(0, 0, 0), color(255, 0, 255), color(0, 0, 0), color(255, 255, 255)],
+    [color(255, 0, 0), color(0, 0, 0), color(0, 0, 0), color(255, 255, 0)]
+  ];
+  handsfree = new Handsfree({
+    showDebug: true,
+    hands: true
+  });
+  handsfree.enablePlugins('browser');
+  handsfree.plugin.pinchScroll.disable();
   
-  //Adding a mouse pressed event listener to the button 
-  button.mousePressed(handleButtonPress); 
- 
-  background(255);
-  noStroke();
+  buttonStart = createButton('Start WebcameraCatch');
+  buttonStart.class('handsfree-show-when-stopped');
+  buttonStart.class('handsfree-hide-when-loading');
+  buttonStart.mousePressed(() => handsfree.start());
+
+  buttonLoading = createButton('...loading...');
+  buttonLoading.class('handsfree-show-when-loading');
+
+  buttonStop = createButton('Stop Webcam');
+  buttonStop.class('handsfree-show-when-started');
+  buttonStop.mousePressed(() => handsfree.stop());
 }
-
+  
 function draw() {
-
-  if(drawIsOn){
-    fill(r,g,b);
-    circle(mouseX,mouseY,bSize);
-  }
-
+  drawHands();
 }
 
-//we only want to draw if the click is on the canvas not on our GUI
-function canvasMousePressed(){
-  drawIsOn = true;
-}
-
-function mouseReleased(){
-  drawIsOn = false;
-}
-
-function mouseDragged() {
-
-  //don't emit if we aren't drawing on the canvas
-  if(!drawIsOn){
-    return;
-  }
-
-  socket.emit("drawing", {
-    xpos: mouseX / width,
-    ypos: mouseY / height,
-    userR: r,
-    userG: g,
-    userB: b,
-    // userS: bSize / width //scaling brush size to user window
-    userS: bSize 
-  });
-
-}
-
-//make it work on mobile
-function canvasTouchStarted(){
-  drawIsOn = true;
-}
-
-function touchEnded(){
-  drawIsOn = false;
-}
-
-function touchMoved() {
-  if(!drawIsOn){
-    return;
-  }
+function drawHands() {
+  const hands = handsfree.data?.hands;
   
-  socket.emit("drawing", {
-    xpos: mouseX / width,
-    ypos: mouseY / height,
-    userR: r,
-    userG: g,
-    userB: b,
-    // userS: bSize / width //scaling brush size to user window
-    userS: bSize 
-  });
+  if (!hands?.landmarks) return;
   
+  hands.landmarks.forEach((hand, handIndex) => {
+    hand.forEach((landmark, landmarkIndex) => {
+   
+      if (colorMap[handIndex]) {
+        switch (landmarkIndex) {
+          case 8: fill(colorMap[handIndex][0]); break;
+          case 12: fill(colorMap[handIndex][1]); break;
+          case 16: fill(colorMap[handIndex][2]); break;
+          case 20: fill(colorMap[handIndex][3]); break;
+          default: fill(color(255, 255, 255));
+        }                
+      }
+      
+      if (handIndex === 0 && landmarkIndex === 8) {
+        stroke(color(255, 255, 255));
+        strokeWeight(50);
+        circleSize = 40;
+      } else {
+        stroke(color(0, 0, 0));
+        strokeWeight(0);
+        circleSize = 10;
+      }
+      
+      circle(
+        sketch.width - landmark.x * sketch.width,
+        landmark.y * sketch.height,
+        circleSize
+      );
+    });
+  });
 }
-
-
-function onDrawingEvent(data){
-  fill(data.userR,data.userG,data.userB);
-  //circle(data.xpos * width,data.ypos * height,data.userS * width);//scaling brush size to user window
-  circle(data.xpos * width,data.ypos * height,data.userS);//slightly nicer on mobile
-}
-
-function handleButtonPress()
-{
-    gui.toggleClass("open");
-}
-
-function handleSliderInputChange(){
-  r = map(rSlider.value(),0,rSlider.width,0,255);
-  g = map(gSlider.value(),0,gSlider.width,0,255);
-  b = map(bSlider.value(),0,bSlider.width,0,255);
-  bSize = sizeSlider.value();
-
-  colorSwatch.style("background-color","rgb("+r+","+g+","+b+")");
-}
-
-//Events we are listening for
-function windowResized() {
-
-  //wipes out the history of drawing if resized, potential fix, draw to offscreen buffer
-  //https://p5js.org/reference/#/p5/createGraphics
-  // resizeCanvas(windowWidth, windowHeight);
-
-  //ratio fix... but then need to make a bunch of other UX decisions like whether you zoom into the canvas or center it somehow
-  // if(windowWidth > windowHeight){
-  //   resizeCanvas(windowWidth, windowWidth);
-  // }else{
-  //   resizeCanvas(windowHeight, windowHeight);
-  // }
-}
-
-// Connect to Node.JS Server
-socket.on("connect", () => {
-  console.log(socket.id);
-});
-
-// Callback function on the event we disconnect
-socket.on("disconnect", () => {
-  console.log(socket.id);
-});
-
-// Callback function to recieve message from Node.JS
-socket.on("drawing", (data) => {
-  console.log(data);
-
-  onDrawingEvent(data);
-
-});
